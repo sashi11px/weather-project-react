f9762ae723a5aec83f0e6dabd834a6af1a851ad1
sashi11px <djsashi11@gmail.com>
2023-01-10 16:37:59 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div className="App">
-    <h1>Weather App</h1>
-    <footer>
-    This project was created by Sasha Jackson and is{" "}
-<a href="https://github.com/sashi11px/weather-project-react" 
-target="_blank">
-    open-sourced on GitHub
-    </a>   
-</footer>    
-    </p>
-  
-  
-  
-  
-  
-  
-  
-  
-  </div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Sasha Jackson and is{" "}
+          <a
+            href="https://github.com/sashi11px/weather-project-react"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
-
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.css";
 import Search from "./Search";
 
 const rootElement = document.getElementById("root");
